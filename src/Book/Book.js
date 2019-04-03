eed0ee882f66ae0d3d9b648f411d1007132ac47a
kfuquay <kirsty.fuquay@gmail.com>
2019-04-03 15:32:54 -0400

    conditionally render book image - fix for instances where api has no imageLink

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -6,8 +6,8 @@ export default function Book(props) {
         <div className="book">
             <h1 className="book-title">{props.volumeInfo.title}</h1>
 
-            <img className="book-img" src={props.volumeInfo.imageLinks.smallThumbnail} alt='book cover' />
-            
+            {props.volumeInfo.imageLinks ? <img className="book-img" src={props.volumeInfo.imageLinks.thumbnail}alt='book cover' /> : null} 
+             
             <h2 className="author">Author: {props.volumeInfo.authors}</h2>
             
             {props.saleInfo.listPrice ? <h2 className="price">Price : ${props.saleInfo.listPrice.amount}</h2> : <h2>Price: not for sale</h2>}
